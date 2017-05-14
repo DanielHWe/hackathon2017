de15e25ca61cab446d4a77a50439b8b1b4efc2ea
Daniel W <daniel.wehrle@haufe-lexware.com>
2017-05-14 11:42:34 +0200

    Update add to sample page and readme.md filled

--- Hackathon/Hackathon/README.md
+++ Hackathon/Hackathon/README.md
@@ -1,3 +1,20 @@
 ﻿# Hackathon
 
+use "node app.js" to start.
+
+## REST Service
+
+this node application use json-server component to provide a read/write JSON server on Port 3000.
+json-server was included by use of npm. It allow to store and retrieve mixed json date. stored in db.json file.
+
+See bottom of app.js for usage
+
+More under https://github.com/typicode/json-server include filter and other options
+
+## Sample, default page
+
+Default page shows how to use the rest service with jquerry for "Brancheninformationen". 
+Implemented in views/index.jade and public/javascript/main.js
+
+find on http://localhost:1337/ 
 
--- Hackathon/Hackathon/public/javascripts/main.js
+++ Hackathon/Hackathon/public/javascripts/main.js
@@ -1,4 +1,6 @@
-﻿
+﻿//
+//Get data on start from REST service
+//
 $(function() {
 
     var $orders = $('#mylist');
@@ -11,13 +13,13 @@ $(function() {
             $.each(mylist,
                 function(i, mylist) {
                     $orders.append('<tr id="brance_' + mylist.id + '">' +
-                        '<td><input type="text" name="name_' + mylist.id + '" value="' + mylist.name +
-                        '"></input></td><td><input type="text" name="StromProKopf_' + mylist.id + '" value="' + mylist.StromProKopf +
-                        '"></input></td><td><input type="text" name="StromProQM_' + mylist.id + '" value="' + mylist.StromProQM +
-                        '"></input></td><td><input type="text" name="GasProKopf_' + mylist.id + '" value="' + mylist.GasProKopf +
-                        '"></input></td><td><input type="text" name="GasProQM_' + mylist.id + '" value="' + mylist.GasProQM +
-                        '"></input></td><td><input type="text" name="OilProKopf_' + mylist.id + '" value="' + mylist.OilProKopf +
-                        '"></input></td><td><input type="text" name="OilProQM_' + mylist.id + '" value="' + mylist.OilProQM +
+                        '<td><input type="text" id="name_' + mylist.id + '" value="' + mylist.name +
+                        '"></input></td><td><input type="text" id="StromProKopf_' + mylist.id + '" value="' + mylist.StromProKopf +
+                        '"></input></td><td><input type="text" id="StromProQM_' + mylist.id + '" value="' + mylist.StromProQM +
+                        '"></input></td><td><input type="text" id="GasProKopf_' + mylist.id + '" value="' + mylist.GasProKopf +
+                        '"></input></td><td><input type="text" id="GasProQM_' + mylist.id + '" value="' + mylist.GasProQM +
+                        '"></input></td><td><input type="text" id="OilProKopf_' + mylist.id + '" value="' + mylist.OilProKopf +
+                        '"></input></td><td><input type="text" id="OilProQM_' + mylist.id + '" value="' + mylist.OilProQM +
                         '"></input></td><td><button onClick="onUpdate(' + mylist.id + ')">Update</button></td></tr>');
                 });
 
@@ -28,27 +30,33 @@ $(function() {
         }
     });
 
+    //
+    // Add click event to Add button adding new Brance with name
+    //
     $('#add-title').on('click', function() {
-        var titleObj = {
-            title: $title.val(), "StromProKopf": "1",
-            "StromProQM": "1",
-            "GasProKopf": "1",
-            "GasProQM": "1",
-            "OilProKopf": "1",
-            "OilProQM": "1" };
+       
+        // Data are formated as json string because java script objects carshes parser
+        var titleObj = "{\"name\":\"" + $title.val() + "\"," +
+            "\"StromProKopf\":\"1\"," +
+            "\"StromProQM\":\"1\"," +
+            "\"GasProKopf\":\"1\"," +
+            "\"GasProQM\":\"1\"," +
+            "\"OilProKopf\":\"1\"," +
+            "\"OilProQM\":\"1\"}";
 
             $.ajax({
                 type: 'POST',
                 url: 'http://localhost:3000/branchen',
                 data: titleObj,
+                contentType: "application/json",//set content type to use json parser in server
                 success: function(mylist) {
-                    $orders.append('<tr id="brance_' + titleObj.id + '><td><input type="text" name="name_' + titleObj.id+'" value="' + mylist.name +
-                        '"></input></td><td><input type="text" name="StromProKopf_' + mylist.id + '" value="' + titleObj.StromProKopf +
-                        '"></input></td><td><input type="text" name="StromProQM_' + mylist.id + '" value="' + titleObj.StromProQM +
-                        '"></input></td><td><input type="text" name="GasProKopf_' + mylist.id + '" value="' + titleObj.GasProKopf +
-                        '"></input></td><td><input type="text" name="GasProQM_' + mylist.id + '" value="' + titleObj.GasProQM +
-                        '"></input></td><td><input type="text" name="OilProKopf_' + mylist.id + '" value="' + titleObj.OilProKopf +
-                        '"></input></td><td><input type="text" name="OilProQM_' + mylist.id + '" value="' + titleObj.OilProQM +
+                    $orders.append('<tr id="brance_' + titleObj.id + '><td><input type="text" id="name_' + titleObj.id+'" value="' + mylist.name +
+                        '"></input></td><td><input type="text" id="StromProKopf_' + mylist.id + '" value="' + titleObj.StromProKopf +
+                        '"></input></td><td><input type="text" id="StromProQM_' + mylist.id + '" value="' + titleObj.StromProQM +
+                        '"></input></td><td><input type="text" id="GasProKopf_' + mylist.id + '" value="' + titleObj.GasProKopf +
+                        '"></input></td><td><input type="text" id="GasProQM_' + mylist.id + '" value="' + titleObj.GasProQM +
+                        '"></input></td><td><input type="text" id="OilProKopf_' + mylist.id + '" value="' + titleObj.OilProKopf +
+                        '"></input></td><td><input type="text" id="OilProQM_' + mylist.id + '" value="' + titleObj.OilProQM +
                         '"></input><button onClick=("onUpdate(' + mylist.id + ')")) Update /></td></tr>');
                 },
                 error: function (xhr, status, error) {
@@ -62,18 +70,37 @@ $(function() {
 
 });
 
+//
+// Add function used by update Buttons, id is id of entry from REST service
+//
 function onUpdate(id) {
-    var titleObj = {
-        title: $('#name_' + id).val(),
-        "StromProKopf": $('#StromProKopf_' + id).val(),
-        "StromProQM": $('#StromProQM_' + id).val(),
-        "GasProKopf": $('#GasProKopf_' + id).val(),
-        "GasProQM": $('#GasProQM_' + id).val(),
-        "OilProKopf": $('#OilProKopf_' + id).val(),
-        "OilProQM": $('#OilProQM_' + id).val()
-    };
 
-    request = $.ajax({ url: 'http://localhost:3000/branchen/' + id, type: 'POST', data: titleObj });
+    var name = $('#name_' + id);
+
+    // Data are formated as json string because java script objects carshes parser
+    var titleObj = "{\"id\":\""+id+"\","+
+        "\"name\":\""+ name.val()+"\","+
+        "\"StromProKopf\":\""+ $('#StromProKopf_' + id).val() + "\"," +
+        "\"StromProQM\":\"" + $('#StromProQM_' + id).val() + "\"," +
+        "\"GasProKopf\":\"" + $('#GasProKopf_' + id).val() + "\"," +
+        "\"GasProQM\":\"" + $('#GasProQM_' + id).val() + "\"," +
+        "\"OilProKopf\":\"" + $('#OilProKopf_' + id).val() + "\"," +
+        "\"OilProQM\":\"" + $('#OilProQM_' + id).val() + "\"}";
+
+    
+    request = $.ajax({
+        url: 'http://localhost:3000/branchen/' + id,
+        type: 'PATCH',
+        contentType: "application/json",//set content type to use json parser in server
+        data: titleObj,
+        sucess: function (mylist) {
+            alert('updated');
+        },
+        error: function (xhr, status, error) {
+            alert(xhr.responseText);
+
+        }
+    });
 
     request.done(function () {
         alert('updated');
